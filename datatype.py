2eeadb60de7da2a69d15775751435503cc2be966
ankancodingal <ankanghoshcodingal93@gmail.com>
2025-01-03 19:18:33 +0530

    Activity 2

--- datatype.py
+++ datatype.py
@@ -1,22 +1,48 @@
-name = "Penguin"
-age = 15
-is_student = True
-weight = 38.5
+#Activity 1
 
-print("Name :", name)
-print("Data Type of Name is", type(name))
-print("Age :", age)
-print("Data Type of Age is", type(age))
-print("is_student :", is_student)
-print("Data Type of is_student is", type(is_student))
-print("Weight :", weight)
-print("Data Type of weight is", type(weight))
+# name = "Penguin"
+# age = 15
+# is_student = True
+# weight = 38.5
 
-print("\n After Type Casting....")
-age = str(age)
-print(age)
-print("Data Type of age is", type(age))
-weight = int(weight)
-print(weight)
-print("Data Type of Weight is", type(weight))
+# print("Name :", name)
+# print("Data Type of Name is", type(name))
+# print("Age :", age)
+# print("Data Type of Age is", type(age))
+# print("is_student :", is_student)
+# print("Data Type of is_student is", type(is_student))
+# print("Weight :", weight)
+# print("Data Type of weight is", type(weight))
+
+# print("\n After Type Casting....")
+# age = str(age)
+# print(age)
+# print("Data Type of age is", type(age))
+# weight = int(weight)
+# print(weight)
+# print("Data Type of Weight is", type(weight))
+
+#Activity 2
+
+num1 = 45
+num2 = 3
+
+print("Number 1", num1)
+print("Number 2", num2)
+print("Addition :", num1+num2)
+print("Difference :", num1-num2)
+print("Product :", num1*num2)
+print("Division :", num1/num2)
+print("Floor Division :", num1//num2)
+print("Modulus Operation :", num1%num2)
+print("Square :", num2**2)
+print("Square Root :", num1**0.5)
+
+print("Equal ?", num1==num2)
+print("Number 1 greater?", num1>num2)
+print("Number 2 greater?", num1<num2)
+print("Not Equal ?", num1!=num2)
+
+result = num1/2+num2**2+10
+print("Result of given equation is:", result)
 
