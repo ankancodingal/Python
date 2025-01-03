b58bf349b480a87cd624bed1be14cc49eeb6b2a9
ankancodingal <ankanghoshcodingal93@gmail.com>
2025-01-03 21:25:54 +0530

    Activity 3

--- datatype.py
+++ datatype.py
@@ -1,29 +1,3 @@
-#Activity 1
-
-# name = "Penguin"
-# age = 15
-# is_student = True
-# weight = 38.5
-
-# print("Name :", name)
-# print("Data Type of Name is", type(name))
-# print("Age :", age)
-# print("Data Type of Age is", type(age))
-# print("is_student :", is_student)
-# print("Data Type of is_student is", type(is_student))
-# print("Weight :", weight)
-# print("Data Type of weight is", type(weight))
-
-# print("\n After Type Casting....")
-# age = str(age)
-# print(age)
-# print("Data Type of age is", type(age))
-# weight = int(weight)
-# print(weight)
-# print("Data Type of Weight is", type(weight))
-
-#Activity 2
-
 num1 = 45
 num2 = 3
 
@@ -45,4 +19,6 @@ print("Not Equal ?", num1!=num2)
 
 result = num1/2+num2**2+10
 print("Result of given equation is:", result)
+
+
 
