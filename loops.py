49a89ebeb185ef6bcead0892387d7a9b3f7c192d
ankancodingal <ankanghoshcodingal93@gmail.com>
2025-01-03 21:44:04 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,66 @@
+#Activity 1
+# num = 23
+# print("Table of 23")
+# for i in range(1,11):
+#   mul = num*i
+#   print("23 x %d = %d" % (i, mul))
+
+#Activity 2
+# n = int(input("enter number of rows"))
+
+# for i in range(0, n):
+
+# 	for j in range(0, i+1):
+# 		print("*", end=" ")
+
+# 	print("\n")
+
+#Activity 3
+# num = 1
+# sum = 0
+# while(num<=10):
+#   sum = sum+num
+#   num = num+1
+
+# print("Sum of First 10 Natural Numbers :", sum)
+
+#Activity 4
+# num = int(input("Enter number to be checked :"))
+
+# flag = False
+
+# if num > 1:
+#     # check for factors
+#     for i in range(2, num):
+#         if (num % i) == 0:
+#             flag = True
+#             break
+
+# if flag:
+#     print(num, "is not a prime number")
+# else:
+#     print(num, "is a prime number")
+
+#ACP
+# # take input from the user
+# num = int(input("Enter a number: "))
+
+# # initialize sum
+# sum = 0
+
+# # find the sum of the cube of each digit
+# temp = num
+# while temp > 0:
+#    digit = temp % 10
+#    sum += digit ** 3
+#    temp //= 10
+
+# # display the result
+# if num == sum:
+#    print(num,"is an Armstrong number")
+# else:
+#    print(num,"is not an Armstrong number")
+
+
+
+	
